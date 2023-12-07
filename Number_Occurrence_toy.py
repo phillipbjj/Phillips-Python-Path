dfa198e5170223f518315cfec1873e82785ca1a2
phillipbjj <pjdempsey3@gmail.com>
2023-12-07 17:14:36 -0600

    almost to the answer, toy_tester work

--- Number_Occurrence_toy.py
+++ Number_Occurrence_toy.py
@@ -9,13 +9,15 @@
 
 #evenOccurrence = [2, 3, 4, 5 ,6, 7, 4, 7, 2, 2, 2]
 #evenOccurrence = [1, 3, 43, 5 ,6, 7, 4, 0, 8, 9, 2]
-evenOccurrence = [1, 7, 2, 4, 5, 6, 8, 9, 6, 4]
+evenOccurrence = [1, 7, 2, 7, 5, 6, 8, 9, 6, 7]
 
 def occurrenceFinder(evenOccurrence):
     for i in evenOccurrence: 
          evenOccurrence.count(i)
-         if evenOccurrence.count(i) % 2 == 0:
-             return i
+         if evenOccurrence.count(i) == evenOccurrence[0]:
+             return evenOccurrence[i]
+         else:
+            return occurrenceFinder(evenOccurrence[1:])
         
 if occurrenceFinder(evenOccurrence) == None:
     print("Null")
--- toy tester.py
+++ toy tester.py
@@ -1,16 +1,19 @@
-#TESTING
-#evenOccurrence = [2, 3, 4, 5 ,6, 7, 4, 7, 2, 2, 2]
+evenOccurrence = [2, 3, 4, 5 ,6, 7, 4, 7, 2, 2, 2]
 #evenOccurrence = [1, 3, 43, 5 ,6, 7, 4, 0, 8, 9, 2]
-evenOccurrence = [1, 7, 2, 4, 5, 6, 8, 9, 6, 4]
+#evenOccurrence = [1, 7, 2, 7, 5, 6, 8, 9, 6, 7]
+emptyList = []
 
-def occurrenceFinder(evenOccurrence):
-    for i in evenOccurrence: 
-         evenOccurrence.count(i)
-         if evenOccurrence.count(i) % 2 == 0:
-             return i
-        
-if occurrenceFinder(evenOccurrence) == None:
-    print("Null")
-else:
-    print(occurrenceFinder(evenOccurrence))    
+def occurrenceFinder(evenList, i):
+        if len(evenList) == 0:
+            return print("Empty list")
+        elif evenList.count(evenList[0]) == i:
+            return emptyList.append(evenList[0]) #return evenList[0]
+        #elif evenList.count(evenList[0]) != None:
+            #return print("Null")
+        else:
+            return occurrenceFinder(evenList[1:], i)
+         
+print(occurrenceFinder(evenOccurrence, 2))
 
+#need to add another list or a way to keep track of the numbers with 1 or more copies
+#and get the value of the first
