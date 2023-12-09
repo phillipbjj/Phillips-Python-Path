54892614953528e8dd656110460642ec349a2d16
phillipbjj <pjdempsey3@gmail.com>
2023-12-09 11:14:56 -0600

    so close will finish today

--- Number_Occurrence_toy.py
+++ Number_Occurrence_toy.py
@@ -7,22 +7,37 @@
  #* print(onlyEven); //  4
 
 
-#evenOccurrence = [2, 3, 4, 5 ,6, 7, 4, 7, 2, 2, 2]
-#evenOccurrence = [1, 3, 43, 5 ,6, 7, 4, 0, 8, 9, 2]
-evenOccurrence = [1, 7, 2, 7, 5, 6, 8, 9, 6, 7]
+#evenOccurrence = [2, 3, 5, 5 ,6, 7, 5, 7, 2, 2, 2]
+evenOccurrence = [1, 3, 43, 5 ,6, 7, 4, 0, 8, 9, 2]
+#evenOccurrence = [1, 7, 2, 7, 5, 6, 8, 9, 6, 7]
+evenDict = dict()
+def occurrenceFinder(evenList):
+    #evenDict = dict()
+    for i in (evenList):
+        evenDict[i] = evenDict.get(i, 0) + 1
+    return evenDict
+        #print(i)
+         #if evenList == evenList[0]:
+             ##else:
+            #return occurrenceFinder(evenList[1:])
+print(occurrenceFinder(evenOccurrence))
 
-def occurrenceFinder(evenOccurrence):
-    for i in evenOccurrence: 
-         evenOccurrence.count(i)
-         if evenOccurrence.count(i) == evenOccurrence[0]:
-             return evenOccurrence[i]
-         else:
-            return occurrenceFinder(evenOccurrence[1:])
-        
-if occurrenceFinder(evenOccurrence) == None:
-    print("Null")
-else:
-    print(occurrenceFinder(evenOccurrence))
-    
+quit()   
+#if occurrenceFinder(evenOccurrence) == None:
+    #print("Null")
+#else:
+    #print(occurrenceFinder(evenOccurrence))
+    #dictionary = dict()
+		#keys = ['hehe', 'haha', 'suicidal']
+		#for key in keys :
+			#dictionary[key] = dictionary.get(key, 0) + 1
+		#print(keys)
+#if evenList == evenList[0]:
+        #return evenList.count(i)
+         #else:
+            #return occurrenceFinder(evenList[1:])
 
-
+#if evenOccurrence[i] < evenOccurrence[i] and evenOccurrence.count(i) == 2
+#check if they appear more than once
+#if they do, put them into another list
+#compare their positions of those numbers within the first list
--- toy tester.py
+++ toy tester.py
@@ -3,6 +3,29 @@ evenOccurrence = [2, 3, 4, 5 ,6, 7, 4, 7, 2, 2, 2]
 #evenOccurrence = [1, 7, 2, 7, 5, 6, 8, 9, 6, 7]
 emptyList = []
 
+
+def occurrenceFinder(evenList):
+    emptyList = []
+    for i, listNum in enumerate(evenList):
+        #print(i, listNum)
+        evenList.count(listNum)
+        if evenList.count(listNum) == 2:
+            return emptyList.append(listNum)#,  occurrenceFinder(evenList[1:])
+        else: 
+            return occurrenceFinder(evenList[1:])
+print(occurrenceFinder(evenOccurrence))        
+quit()
+def occurrenceFinder(evenList):
+    evenList = []
+    emptyList = []
+    for i in (evenList):
+         evenOccurrence.count(i)
+         if evenOccurrence.count(i) == evenOccurrence[i]:
+             return evenOccurrence[i]
+         else:
+            return occurrenceFinder(evenOccurrence[1:])
+
+quit()
 def occurrenceFinder(evenList, i):
         if len(evenList) == 0:
             return print("Empty list")
@@ -17,3 +40,6 @@ print(occurrenceFinder(evenOccurrence, 2))
 
 #need to add another list or a way to keep track of the numbers with 1 or more copies
 #and get the value of the first
+#check if they appear more than once
+#if they do, put them into another list
+#compare their positions of those numbers within the first list
