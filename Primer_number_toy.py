25d3c17690df90fddf6fecaccf5e2725295754e5
phillipbjj <pjdempsey3@gmail.com>
2023-12-14 13:33:02 -0600

    start to prime number toy

---
+++ Primer_number_toy.py
@@ -0,0 +1,6 @@
+ #* A prime number is a whole number that has no other divisors other than
+ #* itself and 1. Write a function that accepts a number and returns true if it's
+ #* a prime number, false if it's not.
+x = input()
+
+print(x)
