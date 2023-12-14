bf3c908d13764193f2ff6a1bd98cef23a8713741
phillipbjj <pjdempsey3@gmail.com>
2023-12-14 15:16:38 -0600

    works "most" of the time

--- Primer_number_toy.py
+++ Primer_number_toy.py
@@ -1,6 +1,16 @@
  #* A prime number is a whole number that has no other divisors other than
  #* itself and 1. Write a function that accepts a number and returns true if it's
  #* a prime number, false if it's not.
-x = input()
+#primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+primes = list(range(1, 101))
 
-print(x)
+x = 7
+def primeNumberFunction(x):
+    for numba in range(2, 10):
+        if x != numba and x % numba == 0:
+            return "False"
+        elif (x / x == 1) or (x / 1 == x):
+            return "True"
+    
+    
+print(primeNumberFunction(x))
