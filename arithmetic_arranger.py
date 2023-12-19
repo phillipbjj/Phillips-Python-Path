91b5805d401f6fa9c20abfd770ee1b25d627e724
phillipbjj <pjdempsey3@gmail.com>
2023-12-19 09:59:07 -0600

    making prog

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,21 +1,27 @@
 
 mathProblems = ["50 + 50", "1007 + 1337", "123 + 321", "365 - 24"]
+def arithmetic_arranger(mathProblems):
+    if len(mathProblems) > 5:
+        return "Too many problems"
+    elif any("*" in mathProblems or "/" in mathProblems for problem in mathProblems):
+        return "Only addition or subtraction"
+    else:
+        return print(mathProblems[0], mathProblems[1], mathProblems[2], mathProblems[3])
+        #for problem in mathProblems[0:]:
 
-for problem in mathProblems[0:]:
-    arithmetic.append(int(problem))
-print 
-
+arithmetic_arranger(mathProblems)
+        
+        
+    
+quit() 
+print(mathProblems[0], mathProblems[1], mathProblems[2], mathProblems[3]) 
+#print(" ",mathProblems[0],"  ",mathProblems[1],"  ",mathProblems[2],"  ",mathProblems[3])
 stop = 4
-
-
-for a in num_top[:]:
-    int_top.append(int(a))
-print(" ",int_top[0],"  ",int_top[1],"  ",int_top[2],"  ",int_top[3])
-
-
-
-
-print("-----","","-----","","-----","","-----")
+#print()
+strippedProblems = mathProblems[0].split()
+print(strippedProblems[0].rjust(6))
+print(strippedProblems[1], strippedProblems[2].rjust(6))
+print("------","","------","","------","","------")
 
 
 
