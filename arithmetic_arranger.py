5b1dfd674ee4e4ebf86490d236fd555525e1f3d5
phillipbjj <pjdempsey3@gmail.com>
2023-12-18 21:15:38 -0600

    HEH

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,23 +1,11 @@
 
+mathProblems = ["50 + 50", "1007 + 1337", "123 + 321", "365 - 24"]
 
+for problem in mathProblems[0:]:
+    arithmetic.append(int(problem))
+print 
 
-
-
-
-
-
-
-
-
-
-
-
-
-num_top = ["100", "200", "300", "400"]
-num_bot = ["10", "20", "30", "40"]
 stop = 4
-int_top = []
-int_bot = []
 
 
 for a in num_top[:]:
@@ -25,12 +13,9 @@ for a in num_top[:]:
 print(" ",int_top[0],"  ",int_top[1],"  ",int_top[2],"  ",int_top[3])
 
 
-for b in num_bot[:]:
-    int_bot.append(int(b))
-print("+ ",int_bot[0]," + ",int_bot[1]," + ",int_bot[2]," + ",int_bot[3])
+
 
 print("-----","","-----","","-----","","-----")
-
 
 
 
