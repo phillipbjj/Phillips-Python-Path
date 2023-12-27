757f58db9c1b8a16459ecd2a9ae1ba5b6985243a
phillipbjj <pjdempsey3@gmail.com>
2023-12-27 07:49:36 -0600

    start of binarySearch

---
+++ binarySearch.py
@@ -0,0 +1,14 @@
+# Given a sorted list, find the index of an element
+# using a binary search algorithm.
+#
+# Example usage:
+#
+# index = binarySearch([1, 2, 3, 4, 5], 4);
+# print(index); // 3
+# index = binarySearch([1, 2, 3, 4, 5], 8);
+# print(index); // null
+
+
+def binarySearch(array, target) {
+  # write your algorithm logic here
+};
