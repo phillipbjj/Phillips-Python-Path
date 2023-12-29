68bec8e12f04de4a2ab7a5fdff14a89c0fd7134f
phillipbjj <pjdempsey3@gmail.com>
2023-12-29 12:31:22 -0600

    almost done

--- binarySearch.py
+++ binarySearch.py
@@ -1,4 +1,4 @@
-"""# Given a sorted list, find the index of an element
+# Given a sorted list, find the index of an element
 # using a binary search algorithm.
 #
 # Example usage:
@@ -7,11 +7,9 @@
 # print(index); // 3
 # index = binarySearch([1, 2, 3, 4, 5], 8);
 # print(index); // null
-
-
-def binarySearch(array, target) {
+#def binarySearch(array, target) {
   # write your algorithm logic here
-}"""
+#}
 
 """def binary_search(nums,target,low,high):
   if low > high:
@@ -23,8 +21,8 @@ def binarySearch(array, target) {
     elif nums[mid] < target:
       return binary_search(nums,target,mid+1,high)
     else:
-      return binary_search(nums,target,low,mid-1)"""
-"""    nums = [2,5,7,11,14,21,27,30,36]
+      return binary_search(nums,target,low,mid-1)
+    nums = [2,5,7,11,14,21,27,30,36]
 target = 27
 
 print(binary_search(nums,target,0,len(nums)-1))
@@ -32,15 +30,19 @@ print(binary_search(nums,target,0,len(nums)-1))
 
 target = 38
 print(binary_search(nums,target,0,len(nums)-1))
-# Output: False"""
+# Output: False """
 
 def binarySearch(array, target):
-    for number in array:
-        if number == target:
-            return array.index(number)
+  first = array[0]
+  last = len(array) - 1
+  mid = (first + last) // 2
+
+  if array[mid] == target:
+    return array.index(mid)
+  elif 
         
     
-    return "Null"
+  return "Null"
         
 index = binarySearch([1, 2, 3, 4, 5], 4)
 #index = binarySearch([1, 2, 3, 4, 5], 8)
