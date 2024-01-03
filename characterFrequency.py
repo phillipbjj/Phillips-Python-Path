c32dc83ddef4fd88a01d1e81574fe01a7097f693
phillipbjj <pjdempsey3@gmail.com>
2024-01-03 11:38:20 -0600

    with comments

--- characterFrequency.py
+++ characterFrequency.py
@@ -18,12 +18,13 @@
 
 def characterFrequency(string):
     letterDict = {}
+    # Counts letters within the string and adds to dictionary
     for letter in string:
         for single in letter:
             letterDict[single] = letterDict.get(single, 0) + 1
-            
+    # Convert the dictionary to a list of tuples        
     organizedList = [[key, value] for key, value in letterDict.items()]
-     
+    # Sort the list of tuples using selection sort 
     for a in range(len(organizedList)):
         min_index = a
         for b in range(a+1, len(organizedList)):          
@@ -31,6 +32,8 @@ def characterFrequency(string):
                 min_index = b
             elif organizedList[min_index][1] == organizedList[b][1] and organizedList[min_index][0] > organizedList[b][0]:
                 min_index = b
+        #After the inner loop finishes, we swap the tuple at a with the tuple at min_index. 
+        #This places the tuple with the highest frequency and earliest alphabetical character among the unsorted tuples at index a.        
         organizedList[a], organizedList[min_index] = organizedList[min_index], organizedList[a]
                       
     return  organizedList
