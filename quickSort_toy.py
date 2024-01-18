fa89b6c84f1eb84d349e0e2023de9ca748cfe77e
phillipbjj <pjdempsey3@gmail.com>
2024-01-18 13:46:45 -0600

    parition completed

--- quickSort_toy.py
+++ quickSort_toy.py
@@ -1,20 +1,30 @@
 def quickSort(array, low = None, high = None):
-
-    partition1 = []
-    partition2 = []
+    
     if low is None:
         low = 0
     if high is None:
         high = len(array)
-    pivot = array[0]
-    indexer = low - 1
+    pivot = low
+    storageIndex = pivot + 1
     for increment in range(low, high):
-        if array[increment] >= pivot:        
-            partition2.append(array[increment])
-        elif array[increment] < pivot:
-            partition1.append(array[increment])     
+        if array[increment] < array[pivot]:  
+            array[increment], array[storageIndex] = array[storageIndex], array[increment]
+            storageIndex += 1          
+    array[pivot], array[storageIndex - 1] = array[storageIndex - 1], array[pivot]
     
-    return partition1
+    if low < high:
+        pivot = array[0]
+        quickSort(array, pivot + 1, high)
+        quickSort(array, high, pivot - 1)
+    """for increment in range(low, high):
+        if array[increment] < array[pivot]:
+            array[increment], array[increment + 1] = array[increment + 1], array[increment]
+            return quickSort(array, pivot, pivot + 1, high)
+        elif array[increment] > array[pivot]:
+            #array[increment], array[increment + 1] = array[increment + 1], array[increment]
+             return quickSort(array, pivot, high, pivot - 1)"""
+    
+    return array
 print(quickSort([27, 38, 12, 39, 29, 16]))
 
 """for each (unsorted) partition
