9d5e6c4657534a5bde5587e61712edb277275135
phillipbjj <pjdempsey3@gmail.com>
2024-01-19 12:44:14 -0600

    quickSort complete :)

--- quickSort_toy.py
+++ quickSort_toy.py
@@ -6,24 +6,16 @@ def quickSort(array, low = None, high = None):
         high = len(array)
     pivot = low
     storageIndex = pivot + 1
-    for increment in range(low, high):
-        if array[increment] < array[pivot]:  
-            array[increment], array[storageIndex] = array[storageIndex], array[increment]
-            storageIndex += 1          
-    array[pivot], array[storageIndex - 1] = array[storageIndex - 1], array[pivot]
-    
+
     if low < high:
-        pivot = array[0]
-        quickSort(array, pivot + 1, high)
-        quickSort(array, high, pivot - 1)
-    """for increment in range(low, high):
-        if array[increment] < array[pivot]:
-            array[increment], array[increment + 1] = array[increment + 1], array[increment]
-            return quickSort(array, pivot, pivot + 1, high)
-        elif array[increment] > array[pivot]:
-            #array[increment], array[increment + 1] = array[increment + 1], array[increment]
-             return quickSort(array, pivot, high, pivot - 1)"""
-    
+        for increment in range(low, high):
+            if array[increment] < array[pivot]:  
+                array[increment], array[storageIndex] = array[storageIndex], array[increment]
+                storageIndex += 1         
+        array[pivot], array[storageIndex - 1] = array[storageIndex - 1], array[pivot]
+        quickSort(array, low, storageIndex - 1)
+        quickSort(array, storageIndex, high)
+  
     return array
 print(quickSort([27, 38, 12, 39, 29, 16]))
 
