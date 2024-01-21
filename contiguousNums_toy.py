0a069d769075a75216efd59fd0b074bc4c60b2f5
phillipbjj <pjdempsey3@gmail.com>
2024-01-20 22:11:09 -0600

    round2

--- contiguousNums_toy.py
+++ contiguousNums_toy.py
@@ -1,23 +1,29 @@
 """ * Given an array of numbers, calculate the greatest contiguous sum of numbers in it.
  * A single array item will count as a contiguous sum.
- * example 1: sumArray([1, 2, 3]); // => 6
- * example 2: sumArray([1, 2, 3, -4]); // 6
- * example 3: sumArray([1, 2, 3, -4, 5]); // 7
- * example 4: sumArray([4, -1, 5]); // => 8
- * example 5: sumArray([10, -11, 11]); // 11
+ * example 1: sumArray([""1, 2, 3 ""]); // => 6
+ * example 2: sumArray([""1, 2, 3"", -4]); // 6
+ * example 3: sumArray([""1, 2, 3, -4, 5""]); // 7
+ * example 4: sumArray([""4, -1, 5""]); // => 8
+ * example 5: sumArray([10, -11, ""11""]); // 11
 // Solved in O(n) time with O(1) memory
 var sumArray = function(array) {
 };"""
 
 def sumArray(array):
-    
+    sumCount = array[0]
+    biggestSumArray = []
+    biggestSum = array[0]
+    arraySpan = len(array)
     for num in array:
-        
-        return array
+        if (array[num] + array[num + 1]) > sumCount:
+            biggestSumArray.append((array[num] + array[num+1]))
+            sumCount += 1
+
+        return biggestSum
 
 
 
-print(sumArray([1, -2, 3, 10, -4, 7, 2, -5]))
+print(sumArray([1, -2, 3, 10, -4, 7, 2, -5]))       #[3, 10, -4, 7, 2]-> 18
 """The idea is to iterate through the array, keeping track of the current sum and
 updating it based on whether adding the next element increases the sum or not.
 The maximum sum encountered during the iteration will be the answer."""
