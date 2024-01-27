77dce536237f3301661c9a4e56531f9ecda9f5e6
phillipbjj <pjdempsey3@gmail.com>
2024-01-27 08:34:03 -0600

    finished with help

--- contiguousNums_toy.py
+++ contiguousNums_toy.py
@@ -10,33 +10,29 @@ var sumArray = function(array) {
 };"""
 #num + 1 < len(array) and num - 1 > len(array) and
 def sumArray(array):
-    biggestSum = 0
-    currentSum = 0
-    
-    for num in range(len(array)):
-        #if array[num] > 0:
-        if array[num] >= biggestSum:
-            currentSum = array[num]
-          
-        if num + 1 > len(array) and (array[num] + array[num + 1]) > biggestSum:
-            currentSum = currentSum + array[num + 1]
-            if num + 1 < len(array) and array[num + 1] < 0:
-                if num + 2 < len(array) and (array[num + 1] + array[num + 2]) > 0:
-                    currentSum = currentSum + (array[num + 1] + array[num + 2])
-            #elif num + 1 < len(array) and array[num + 1] > 0: 
-                
-        if num - 1 >= 0 and array[num] + array[num - 1] < biggestSum:
-            currentSum = currentSum + array[num - 1]
-            if array[num - 2] + array[num - 1] > 0:
-                currentSum = currentSum + array[num - 2]   
-                          
-        if biggestSum <= currentSum:
-            biggestSum = currentSum   
+    biggestSum = array[0]
+    currentSum = array[0]
+
+    for num in array[1:]:
+        if currentSum + num > num:
+            currentSum = currentSum + num
+        else:
+            currentSum = num
+
+        if currentSum > biggestSum:
+            biggestSum = currentSum
         
-        currentSum = 0   
-    return biggestSum
+    return biggestSum  
 
 print(sumArray([1, -2, 3, 10, -4, 7, 2, -5]))       #[3, 10, -4, 7, 2]-> 18
 """The idea is to iterate through the array, keeping track of the current sum and
 updating it based on whether adding the next element increases the sum or not.
 The maximum sum encountered during the iteration will be the answer."""
+
+"""currentSum is the maximum sum ending at the current position.
+biggestSum is the maximum sum we’ve seen so far.
+For each number in the array (starting from the second number), we update currentSum to be the current number if the sum of the current number and 
+the previous currentSum is less than the current number. Otherwise, we add the current number to currentSum.
+If currentSum is larger than biggestSum, we update biggestSum.
+Finally, we return biggestSum which is the largest contiguous sum in the array.
+This approach ensures that we’re always considering the largest sum that includes the current number, and we keep track of the largest sum we’ve seen so far."""
