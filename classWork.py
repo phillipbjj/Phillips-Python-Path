71bbce53b67db0253cfd42ab88658aa67f81e871
phillipbjj <pjdempsey3@gmail.com>
2024-01-29 17:32:03 -0600

    start

--- classWork.py
+++ classWork.py
@@ -1,1 +1,11 @@
 #class work
+
+class bjjFighter:
+    def __init__(self, height, age, weight, rank):
+        self.height = height
+        self.age = age
+        self.weight = weight
+        self.rank = rank
+    
+    def bjjAthlete(self):
+        
