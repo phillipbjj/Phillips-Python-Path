3c7f812d3770d3720d045d9522109c6584d5f21f
phillipbjj <pjdempsey3@gmail.com>
2024-01-30 17:57:45 -0600

    organize fighters

--- classWork.py
+++ classWork.py
@@ -21,23 +21,44 @@ fighters_dict = {'Fighter1': fighter1, 'Fighter2': fighter2}
 #So, you would just use the assignment operator (=), like dictionary[key] = instance.
 
 class bjjFighter:
+    
+    RANKS = {'White': 1, 'Blue': 2, 'Purple': 3, 'Brown': 4, 'Black': 5}
     def __init__(self, rank, age, weight, height):
-        self.rank = rank
+        self.rank_value = self.RANKS[rank]
+        self.rank_color = rank
         self.age = age
         self.weight = weight
         self.height = height
     def __str__(self):
-        return f'Rank: {self.rank}, Age: {self.age}, Weight: {self.weight} lbs, Height: {self.height} in'
-
-#Create instances for each athlete   
-athlete1 = bjjFighter('Blue', 26, 175, 68)
-athlete2 = bjjFighter('Blue', 22, 180, 73)
+        return f'Rank: {self.rank_color}, Age: {self.age}, Weight: {self.weight} lbs, Height: {self.height} in'
 
 tournamentSignups = {}
-#Add the athletes to the dictionary
+tournamentSignups['Phillip Dempsey'] = bjjFighter('Blue', 26, 160, 68)
+tournamentSignups['Jack Howell'] = bjjFighter('Blue', 22, 180, 73)
+tournamentSignups['Martin Helouin'] = bjjFighter('Blue', 20, 160, 68)
+tournamentSignups['Aaron Taylor'] = bjjFighter('White', 25, 255, 70)
+tournamentSignups['Jayke Lebeau'] = bjjFighter('White', 26, 185, 72)
+tournamentSignups['Travis Sutton'] = bjjFighter('Purple', 28, 145, 64)
+tournamentSignups['Caleb Gilmore'] = bjjFighter('Brown', 28, 180, 70)
+tournamentSignups['Gordan Ryan'] = bjjFighter('Black', 28, 240, 74)
+tournamentSignups['Josh Mancuso'] = bjjFighter('Black', 40, 185, 68)
+tournamentSignups['Steven Bransford'] = bjjFighter('Purple', 30, 210, 68)
+tournamentSignups['Nick Patrick'] = bjjFighter('Purple', 28, 190, 69)
+tournamentSignups['George Munoz'] = bjjFighter('Blue', 34, 160, 66)
+tournamentSignups['Brian Miller'] = bjjFighter('Purple', 22, 160, 70)
+tournamentSignups['Tyler Stanczak'] = bjjFighter('White', 29, 145, 72)
 
-tournamentSignups['Phillip Dempsey'] = athlete1
-tournamentSignups['Jack Howell'] = athlete2
+class tournamentOrganizer:
+    
+    def __init__(self, signups):
+        self.signups = signups
+#Create instances for each athlete  and add the athletes to the dictionary
 
-print(tournamentSignups['Phillip Dempsey'])     
-print(tournamentSignups['Jack Howell'])    
+
+#tournamentSignups['Phillip Dempsey'] = athlete1
+
+#print(tournamentSignups['Phillip Dempsey'])  
+#Used to print fighters names [keys] along with their values {attributes from class} 
+"""for name, fighter in tournamentSignups.items():   
+    print(f'{name}: {fighter}')  """
+
