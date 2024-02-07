cf20c300c2d5e3b1f0dc3782587ec5fe1a64a7c9
phillipbjj <pjdempsey3@gmail.com>
2024-02-07 16:01:40 -0600

    start

---
+++ binaryTree_work.py
@@ -0,0 +1,9 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+class BinaryTree(object):
+    def __innit__(self, root):
+        self.root = Node(root)
