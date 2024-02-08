e01a5c85987191c063e6aa175d3f6217695bcfad
phillipbjj <pjdempsey3@gmail.com>
2024-02-08 16:03:35 -0600

    traversed: preorder, inorder, postorder

--- binaryTree_work.py
+++ binaryTree_work.py
@@ -7,7 +7,7 @@ class Node(object):
         
 #Establishes the root of my tree as a Node object.      
 class BinaryTree(object):
-    def __innit__(self, root):
+    def __init__(self, root):
         self.root = Node(root)
         
     #Helper function for printing traversal
@@ -16,7 +16,13 @@ class BinaryTree(object):
     def print_tree(self, traversal_type):
         if traversal_type == "preorder":
             return self.preorder_print(tree.root, "") 
-        
+        elif traversal_type == "inorder":
+            return self.inorder_print(tree.root, "")
+        elif traversal_type == "postorder":
+            return self.postorder_print(tree.root, "")
+        else: 
+            print("Traversal type" + str(traversal_type) + "is not supported.")
+            return False       
     #Traverses my binary tree in pre-order    
     def preorder_print(self, start, traversal):
         """Root->Left->Right"""
@@ -26,14 +32,31 @@ class BinaryTree(object):
             traversal = self.preorder_print(start.right, traversal) #Recursive call down right side of tree
         return traversal
     
+    def inorder_print(self, start, traversal):
+        """Left->Root->Right"""
+        if start:
+            traversal = self.inorder_print(start.left, traversal)
+            traversal += (str(start.value) + "-")
+            traversal = self.inorder_print(start.right, traversal)
+        return traversal
+    
+    def postorder_print(self, start, traversal):
+        """Left->Right->Root"""
+        if start:
+            traversal = self.postorder_print(start.left, traversal)
+            traversal = self.postorder_print(start.right, traversal)
+            traversal += (str(start.value) + "-")
+        return traversal
+# PREORDER = 1-2-4-5-3-6-7
+# INORDER = 4-2-5-1-6-3-7  
+# POSTORDER = 4-2-5-6-3-7-1 
 #          1
 #        /   \
 #       2     3
 #      / \   / \
 #     4   5 6   7
-#                \
-#                 8
-      
+
+# Establishes my tree      
 tree = BinaryTree(1)
 tree.root.left = Node(2)
 tree.root.right = Node(3)
@@ -41,6 +64,7 @@ tree.root.left.left = Node(4)
 tree.root.left.right = Node(5)
 tree.root.right.left = Node(6)
 tree.root.right.right = Node(7)
-tree.root.right.right.right = Node(8)
-
+#print(tree.print_tree("preorder"))
+#print(tree.print_tree("inorder"))
+print(tree.print_tree("postorder"))
         
