1fad83c58f5256d4221569918d9c6aa80d83e849
phillipbjj <pjdempsey3@gmail.com>
2024-02-20 11:57:02 -0600

    app.py

---
+++ TwomPro_Chat_Endpoint.py
@@ -0,0 +1,11 @@
+"""Web address: pjdempsey3.pythonanywhere.com"""
+
+from flask import Flask, request        #Imports and initializes my flask application
+app = Flask(__name__)
+                                        #Defines a route in Flask using @app.route() decorator.
+@app.route('/chat', methos=['POST'])    #/chat is the endpoint that accepts POST requests.
+def chat():                             #Function that returns the rponse for that route
+    message = request.form["message"]   #request.form['message'] gets message from POST request.
+    
+    response = "Message eceived hehe :3: " + message
+    return response
