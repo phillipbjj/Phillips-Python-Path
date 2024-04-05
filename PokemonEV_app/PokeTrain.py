4e3d9ca0a5c11c58f4009a58bb46b0db54b68d32
phillipbjj <pjdempsey3@gmail.com>
2024-04-04 21:31:20 -0500

    start of app

---
+++ PokemonEV_app/PokeTrain.py
@@ -0,0 +1,4 @@
+import csv
+import ev_reader import Pokemon import load_data
+
+print(load_data)
--- PokemonEV_app/ev_reader.py
+++ PokemonEV_app/ev_reader.py
@@ -1,8 +1,18 @@
 import csv
-from pokemonevs import Pokemon
-
+#This line imports the Pokemon class from the pokemonevs module. This class is used to create Pokemon objects.
+from pokemonevs import Pokemon 
+#This line defines a function called load_data that takes one argument: filename.
+# This function is used to load data from a CSV file.
 def load_data(filename):
+    #This line opens the file with the name filename in read mode ('r'). 
+    #The with statement ensures that the file is properly closed after it is no longer needed.
     with open(filename, 'r') as file:
+        #This line creates a reader object that can iterate over lines in the specified CSV file.
         reader = csv.reader(file)
+        #This line uses a list comprehension to create a list of Pokemon objects.
+        # For each row in the CSV file, it creates a Pokemon object with the values in the row.
+        # The * operator is used to unpack the values from each row.
         data = [Pokemon(*row) for row in reader]
+        #This line returns the list of Pokemon objects.
+    print(data)
     return data
