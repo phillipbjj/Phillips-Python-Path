5db7fa9ad8d14fc4f201dc7134a09789e0ab6910
phillipbjj <pjdempsey3@gmail.com>
2024-04-11 16:19:10 -0500

    index attribute causing issues

---
+++ PokemonEV_app/index#_removal_script.py
@@ -0,0 +1,15 @@
+import csv
+
+# Read the data from the file
+with open('Pokedex_EVs.csv', 'r') as file:
+    reader = csv.reader(file)
+    data = list(reader)
+
+# Remove the '#' from the index numbers
+for row in data:
+    row[0] = row[0].replace('#', '')
+
+# Write the modified data back to the file
+with open('Pokedex_EVs.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
--- PokemonEV_app/pokemonevs.py
+++ PokemonEV_app/pokemonevs.py
@@ -9,6 +9,6 @@ class Pokemon:
         self.sp_defense = sp_defense
         self.speed = speed
         self.total = total
+
     def __repr__(self):
-        return f'Index#:{self.index}, {self.name} HP: {self.hp}, Attack: {self.attack}, Defense: {self.defense}, Sp. Attack: {self.sp_attack}, Sp. Defense: {self.sp_defense}, Speed: {self.speed}, Total EVs: {self.total}'
-    
+        return f'Index: {self.index}, Name: {self.name}, HP: {self.hp}, Attack: {self.attack}, Defense: {self.defense}, Sp. Attack: {self.sp_attack}, Sp. Defense: {self.sp_defense}, Speed: {self.speed}, Total EVs: {self.total}'
