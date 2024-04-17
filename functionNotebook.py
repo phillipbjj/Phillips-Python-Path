7a059dc51645145d8ff787faa0c81404abda72c9
phillipbjj <pjdempsey3@gmail.com>
2024-04-17 10:20:29 -0500

    10/36 testcases passed

--- functionNotebook.py
+++ functionNotebook.py
@@ -1,5 +1,8 @@
 bible = "words of God"
-print(bible.find("God"))
+print(bible.find("God"))    #9
 
 bricked = "BONGO"
-print(bricked.lower())
+print(bricked.lower())      #bongo
+
+belt = "purple"
+print(belt.index("r"))      #2
