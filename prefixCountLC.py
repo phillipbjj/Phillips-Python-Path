7eb72f373550f573dbd5628382fa0f7e57feef8e
phillipbjj <pjdempsey3@gmail.com>
2024-04-18 13:19:37 -0500

    ACCEPTED

--- prefixCountLC.py
+++ prefixCountLC.py
@@ -3,29 +3,29 @@
 #A prefix of a string s is any leading contiguous substring of s
 #Input: words = ["pay","attention","practice","attend"], pref = "at"    Output: 2
 
-
+#RUN TIME: 30MS     MEMORY: 11.80MB
 
 
 def prefixCount(words, pref):
-    fixlength = len(pref)
     prefix_dict = {}
     for word in words:
-        if word[0:fixlength] == pref:
+        if word[0:len(pref)] == pref:
             prefix_dict[word] = prefix_dict.get(word, 0) + 1
         else:
             continue
     
     
-    answer = len(list(prefix_dict))
+    answer = sum(prefix_dict.values())
     
     return answer
     
     
     
     
-    
-words = ["pay","attention","practice","attend"]    
-pref = "at"
+words = ["faeu","fauq","fanc","fap","favkigbbsk","faex","fag","faltrf","fabeckg","faem","fahh","nyyqmdv","faaei","fah","fayr","fazon","fairpv","fanz","fap","fanfxo","fadzmrtjv","famf","faom"]
+pref = "fa"
+#words = ["pay","attention","practice","attend"]    
+#pref = "at"
 print(prefixCount(words, pref))
 
 
