1d150712043c89700c620c46a340553cd571e39e
phillipbjj <pjdempsey3@gmail.com>
2024-05-22 08:06:08 -0500

    leetcode

---
+++ LeetCode/isUglyLC.py
@@ -0,0 +1,18 @@
+#An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+#Given an integer n, return true if n is an ugly number.
+
+def isUgly(n):
+    if n % 2 == 0:
+        return True
+    
+
+
+
+
+
+n = 6
+#n = 1
+#n = 14
+
+
+print(isUgly(n))
