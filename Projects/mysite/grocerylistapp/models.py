183d9b23a566bf1a21b0a790661f753f84ab355f
phillipbjj <pjdempsey3@gmail.com>
2024-06-26 16:19:00 -0500

    added my app to settings(configuring still)

--- Projects/mysite/grocerylistapp/models.py
+++ Projects/mysite/grocerylistapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
 # Create your models here.
+
+class Food(models.Model):
+    food_text = models.CharField(max_length=200)
+    foodadd_date = models.DateTimeField("date added")
+    
+class NonFood(models.Model):
+    nonfood_text = models.CharField(max_length=200)
+    nonfoodadd_date = models.DateTimeField("date added")
+
