88a0c7819525ccf8d2c45dab324c25c85e8c98fc
phillipbjj <pjdempsey3@gmail.com>
2024-06-26 22:21:35 -0500

    started working with the api

--- Projects/mysite/grocerylistapp/models.py
+++ Projects/mysite/grocerylistapp/models.py
@@ -4,8 +4,12 @@ from django.db import models
 class Food(models.Model):
     food_text = models.CharField(max_length=200)
     foodadd_date = models.DateTimeField("date added")
+    def __str__(self):
+        return self.food_text
     
 class NonFood(models.Model):
     nonfood_text = models.CharField(max_length=200)
     nonfoodadd_date = models.DateTimeField("date added")
+    def __str__(self):
+        return self.nonfood_text
 
