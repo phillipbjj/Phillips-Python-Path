fd6f82f7d93ffd935803e417a0a5907836d07ded
phillipbjj <pjdempsey3@gmail.com>
2024-07-01 15:53:32 -0500

    freecodecamp cypher finished

--- Toys/fcc_stringCypher.py
+++ Toys/fcc_stringCypher.py
@@ -1,21 +1,38 @@
 #What if the offset were different for each letter? That would be much more difficult to decrypt.
 #This algorithm is referred to as the Vigenère cipher, where the offset for each letter is determined by another text, called the key.
-text = 'Hello Zaira'
-shift = 3
+text = 'mrttaqrhknsw ih puggrur'
+custom_key = 'happycoding'
 
-def caesar(message, offset):
+def vigenere(message, key, direction=1):
+    key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    encrypted_text = ''
+    final_message = ''
 
     for char in message.lower():
-        if char == ' ':
-            encrypted_text += char
-        else:
-            index = alphabet.find(char)
-            new_index = (index + offset) % len(alphabet)
-            encrypted_text += alphabet[new_index]
-    print('plain text:', message)
-    print('encrypted text:', encrypted_text)
 
-caesar(text, shift)
-caesar(text, 13)
+        # Append any non-letter character to the message
+        if not char.isalpha():
+            final_message += char
+        else:        
+            # Find the right key character to encode/decode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted/decrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    
+    return final_message
+
+def encrypt(message, key):
+    return vigenere(message, key)
+    
+def decrypt(message, key):
+    return vigenere(message, key, -1)
+
+print(f'\nEncrypted text: {text}')
+print(f'Key: {custom_key}')
+decryption = decrypt(text, custom_key)
+print(f'\nDecrypted text: {decryption}\n')
