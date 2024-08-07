20cbf0a25c800b0839be99ce5f5583f7dc43cfad
phillipbjj <pjdempsey3@gmail.com>
2024-08-07 16:32:12 -0500

    added remove groceries function

--- Projects/GroceryApp/main.py
+++ Projects/GroceryApp/main.py
@@ -16,7 +16,7 @@ cursor.execute("SELECT * FROM grocerylist.")  # Query all rows from the groceryl
 rows = cursor.fetchall()
 
 #Add groceries into one of the 2 categories(tables)
-def add_groceries(category, g_item, amount):
+def add_groceries(category, g_item, amount):    #POST
     #Need to add api interaction with website for categories
     connect = sqlite3.connect("grocerylist.db")
     cursor = connect.cursor()
@@ -24,12 +24,11 @@ def add_groceries(category, g_item, amount):
         cursor.execute("INSERT INTO Food (food, amount) VALUES (?, ?)", (g_item, amount))
     elif category == 'Nonfood':
         cursor.execute("INSERT INTO Nonfood (item, amount) VALUES (?, ?)", (g_item, amount))
-    else:
-        raise ValueError("Invalid category. Must be 'Food' or 'Nonfood'.")
+    
     connect.commit()
     connect.close()
 
-def edit_groceries(category, new_g_item, new_amount, current_g_item):
+def edit_groceries(category, new_g_item, new_amount, current_g_item):   #PUT
     #Need to add api interaction with website for categories
     connect = sqlite3.connect("grocerylist.db")
     cursor = connect.cursor()
@@ -37,10 +36,25 @@ def edit_groceries(category, new_g_item, new_amount, current_g_item):
         cursor.execute("UPDATE Food SET food = ?, amound = ?,  WHERE food = ?", (new_g_item, new_amount, current_g_item))
     elif category == 'Nonfood':
         cursor.execute("UPDATE Nonfood SET item = ?, amount = ?, WHERE item = ?", (new_g_item, new_amount, current_g_item))
-    else:
-        raise ValueError("Invalid category. Must be 'Food' or 'Nonfood'.")
+    
     connect.commit()
     connect.close()    
+
+def remove_groceries(category, g_item):
+    connect = sqlite3.connect("grocerylist.db")
+    cursor = connect.cursor()
+    if category == 'Food':
+        cursor.execute("DELETE FROM Food WHERE food = ?", (g_item,))
+    elif category == 'Nonfood':
+        cursor.execute("DELETE FROM Nonfood WHERE item = ?", (g_item,))
+    
+    connect.commit()
+    connect.close()  
+
+
+
+
+
 
 
 def get_grocery_list(self):
