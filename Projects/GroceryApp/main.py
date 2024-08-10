3a712bb98b32a4e365fe6d6adb7ab38233d70c0b
phillipbjj <pjdempsey3@gmail.com>
2024-08-10 17:09:51 -0500

    added self to functions so there were no errors

--- Projects/GroceryApp/main.py
+++ Projects/GroceryApp/main.py
@@ -2,26 +2,26 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import sqlite3
 
-def main():
+
     
     #ADD LAST PRICE FOR AFTER PURCHASE OF GROCERIES, CAN BE TURNED INTO A REPRESENTATION MODEL LATER
-
+def main():
+    
     #Inherits pythons HTTP handler as a subclass for HTTP operations
     class GroceryHandler(BaseHTTPRequestHandler):
         
-        def _set_response(self, status=200):  # Default status is 200 (OK)
+        def _set_response(self, status=200, content_type='application/json'):  # Default status is 200 (OK)
             self.send_response(status)  # Sends the HTTP status code
-            self.send_header('Content-type', 'application/json')  # Specifies that the content is JSON
+            self.send_header('Content-type', content_type)  # Specifies that the content is JSON
             self.end_headers()  # Ends the headers section, ready to send content
         
         #Handling the GET request to view the webpage and database contents
         def do_GET(self):
             #Open and read the index.html file
             with open('index.html', 'rb') as file:
-                self.send_response(200) #Send a 200 OK response
-                self.send_header('Content-type', 'text/html') #Set the content type to HTML
-                self.end_headers() # End the headers
+                self.send_response(200, 'text/html')
                 self.wfile.write(file.read()) #Write the content of the file to the response
+                
         def do_POST(self):
             content_length = int(self.headers['Content-Length'])
             post_data = self.rfile.read(content_length)
@@ -31,7 +31,7 @@ def main():
             g_item = data.get('g_item')
             amount = data.get('amount')
             
-            self.add_groceries(category, g_item, amount)
+            self.add_groceries(category, g_item, amount)    #Performs the actual POST functions purpose
             self._set_response(201)
             self.wfile.write(json.dumps({"message": "Item added successfully!"}).encode('utf-8'))
             
@@ -61,7 +61,7 @@ def main():
             self._set_response()
             self.wfile.write(json.dumps({"message": "Item deleted successfully!"}).encode('utf-8'))    
         #Add groceries into one of the 2 categories(tables)
-        def add_groceries(category, g_item, amount):    #POST
+        def add_groceries(self, category, g_item, amount):    #POST
             #Need to add api interaction with website for categories
             connect = sqlite3.connect("grocerylist.db")
             cursor = connect.cursor()
@@ -73,49 +73,22 @@ def main():
             connect.commit()
             connect.close()
         
-    
+
         #Update current groceries list groceries
-        def edit_groceries(category, new_g_item, new_amount, current_g_item):   #PUT
+        def edit_groceries(self, category, new_g_item, new_amount, current_g_item):   #PUT
             #Need to add api interaction with website for categories
             connect = sqlite3.connect("grocerylist.db")
             cursor = connect.cursor()
             if category == 'Food':
-                cursor.execute("UPDATE Food SET food = ?, amound = ?,  WHERE food = ?", (new_g_item, new_amount, current_g_item))
+                cursor.execute("UPDATE Food SET food = ?, amount = ?,  WHERE food = ?", (new_g_item, new_amount, current_g_item))
             elif category == 'Nonfood':
                 cursor.execute("UPDATE Nonfood SET item = ?, amount = ?, WHERE item = ?", (new_g_item, new_amount, current_g_item))
             
             connect.commit()
             connect.close()  
-        """# Handling the DELETE request to remove groceries
-    def do_DELETE(self):
-        content_length = int(self.headers['Content-Length'])  # Gets the size of the incoming data
-        delete_data = self.rfile.read(content_length)  # Reads the incoming data based on its length
-        data = json.loads(delete_data)  # Decodes the JSON data into a Python dictionary
-
-        # Extracting data from the JSON payload
-        category = data['category']
-        g_item = data['g_item']
         
-        # Connecting to the SQLite database
-        connect = sqlite3.connect("grocerylist.db")
-        cursor = connect.cursor()  # Creating a cursor object to execute SQL commands
-        
-        # Deleting the data from the appropriate table based on the category
-        if category == 'Food':
-            cursor.execute("DELETE FROM Food WHERE food = ?", (g_item,))
-        elif category == 'Nonfood':
-            cursor.execute("DELETE FROM Nonfood WHERE item = ?", (g_item,))
-        
-        # Committing the transaction and closing the connection
-        connect.commit()
-        connect.close()
-        
-        # Sending a response back to the client
-        self._set_response()  # Default status is 200 (OK)
-        self.wfile.write(json.dumps({"message": "Item deleted successfully!"}).encode('utf-8'))"""   
-           
         #Remove groceries from the list
-        def remove_groceries(category, g_item):
+        def remove_groceries(self, category, g_item):
             connect = sqlite3.connect("grocerylist.db")
             cursor = connect.cursor()
             if category == 'Food':
@@ -126,19 +99,10 @@ def main():
             connect.commit()
             connect.close()  
 
+    # Function to start the HTTP server
+    def run(server_class=HTTPServer, handler_class=GroceryHandler, port=8080):
+        server_address = ('', port)  # Setting up the server to listen on all available IP addresses and the specified port
+        httpd = server_class(server_address, handler_class)  # Creating an instance of HTTPServer with the specified address and handler
+        print(f'Starting server on port {port}...')  # Logging the server start
+        httpd.serve_forever()  # Starting the server loop, which will keep running to handle incoming requests
 
-
-
-"""# Function to start the HTTP server
-def run(server_class=HTTPServer, handler_class=GroceryHandler, port=8080):
-    server_address = ('', port)  # Setting up the server to listen on all available IP addresses and the specified port
-    httpd = server_class(server_address, handler_class)  # Creating an instance of HTTPServer with the specified address and handler
-    print(f'Starting server on port {port}...')  # Logging the server start
-    httpd.serve_forever()  # Starting the server loop, which will keep running to handle incoming requests"""
-
-
-    
-    
-    
-if __name__ == "__main__":
-    main()  # Run the server when the script is executed
