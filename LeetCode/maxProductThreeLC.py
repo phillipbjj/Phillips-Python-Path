bbea6f7bac385ce091f17c018bf19e499033cc85
phillipbjj <pjdempsey3@gmail.com>
2025-01-06 09:07:37 -0600

    copilot solved tit

--- LeetCode/maxProductThreeLC.py
+++ LeetCode/maxProductThreeLC.py
@@ -9,25 +9,29 @@ Constraints:
     -1000 <= nums[i] <= 1000
 """
 def maximumProduct(nums):
-    products = []
-    maxProd = 0
-    prod = 1
-    i = 0
+    max1 = max2 = max3 = float('-inf')
+    min1 = min2 = float('inf')
+    
     for num in nums:
-        prod *= num
-        print(prod)
-        products.append(prod)
+        # Update the three largest numbers
+        if num > max1:
+            max3 = max2
+            max2 = max1
+            max1 = num
+        elif num > max2:
+            max3 = max2
+            max2 = num
+        elif num > max3:
+            max3 = num
         
-        
+        # Update the two smallest numbers
+        if num < min1:
+            min2 = min1
+            min1 = num
+        elif num < min2:
+            min2 = num
     
-    
-    
-    
-    
-    
-    return None
-
-
-nums = [-1,2,-3,4] #Outcomes: 
+    return max(max1 * max2 * max3, min1 * min2 * max1)
+nums = [-1,2,5,4] #Outcomes: 
 
 print(maximumProduct(nums))
